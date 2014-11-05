083c7e08e97d469c3c3ebe291811ce07ef984ead
goangit <goangit@gmail.com>
2014-11-06 10:47:23 +1100

    Add sorted completion-ignored-extensions

--- lisp/local/init-local-util.el
+++ lisp/local/init-local-util.el
@@ -1,4 +1,15 @@
+(defun gl/compare-extension-strings (s1 s2)
+  "Alphabetic substring comparison with boolean return required by sort"
+  (let ((tst (compare-strings (downcase s1) (string-match "[a-zA-Z]" s1) nil
+                              (downcase s2) (string-match "[a-zA-Z]" s1) nil)))
+    (when (numberp tst)
+      (setq tst (if (< tst 0) t nil) ))
+    tst))
 
-
+(defun gl/add-completion-ignored-extensions (lst)
+  "Add LST elements to completion-ignored-extensions, return sorted result"
+  (sort (delete-dups
+         (append completion-ignored-extensions lst))
+        'gl/compare-extension-strings))
 
 (provide 'init-local-util)
