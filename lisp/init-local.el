902bb6a96b5db444e6bf9abd2a3b6b14f1d4ca4f
goangit <goangit@gmail.com>
2014-11-05 11:32:40 +1100

    Preload custom directory spec and comment

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,15 +2,16 @@
 ;;; upstream merge conflict potential. Possibly this is not strictly
 ;;; necessary if one understands git better than I do at this point.
 ;;;
-;;; See init-preload-local.el for pre-loaded local customisations.
+;;; See init-preload-local.el for pre-loaded local customisations,
+;;; which include specification of user's local lisp directory.
+;;;
 
 (require-package 'ess)
-
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 (require 'ess-init)
 
 (provide 'init-local)
+
 
 ;; Local Variables:
 ;; coding: utf-8
