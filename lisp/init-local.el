2adc73aebe7ca053d51458a3b368f0f39c946888
Greg <greg@twominds.org>
2015-09-09 09:20:52 +1000

    intial import of org-mode config, needs work

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,21 +1,35 @@
+;;; Package --- Summary
 ;;;
 ;;; Enable arbitrary customised local configuration, while minimising
-;;; upstream merge conflict potential. Possibly this is not strictly
-;;; necessary if one understands git better than I do at this point.
+;;; upstream merge conflict potential.
+;;;
+;;;
+;;; Commentary:
+;;;
+;;; Possibly this is not strictly necessary if one understands git
+;;; better than I do at this point.
 ;;;
 ;;; See init-preload-local.el for pre-loaded local customisations,
 ;;; which include specification of user's local lisp directory.
 ;;;
+;;;
+;;; Code:
+
 
 (require-package 'ess)
+(require-package 'jabber)
+(require-package 'julia-mode)
 
 (require 'init-local-ess)
 (require 'init-local-misc)
 (require 'init-local-python)
 
-(provide 'init-local)
+;; (require 'init-local-org)
 
 ;; Local Variables:
 ;; coding: utf-8
 ;; no-byte-compile: t
 ;; End:
+
+(provide 'init-local)
+;;; init-local.el ends here
